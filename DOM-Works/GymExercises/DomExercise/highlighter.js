d83c779ddafc11c849368cbc793063c7297c1bde
Muhanguzi-boss <bossmuhanguzi@gmail.com>
2024-12-09 18:51:57 +0200

    Final Exercise

--- DOM-Works/GymExercises/DomExercise/highlighter.js
+++ DOM-Works/GymExercises/DomExercise/highlighter.js
@@ -1,1 +1,22 @@
- 
+const element = document.querySelector("#myParagraph").textContent;
+function highlightwords(element){
+    const obj = {};
+    const paragraph = element.split(" ");
+    paragraph.forEach(item => item in obj ? obj[item] ++ : obj[item] = 1)
+    console.log(obj);
+    const entry = Object.entries(obj);
+    const sortedEntries = entry.sort((a,b) => b[1] - a[1]);
+    const top5 = sortedEntries.slice(1, 6);
+    const top5Keys = top5.map(entry => entry[0]);
+    console.log(top5Keys)
+    console.log(paragraph);
+
+    const highlightedText = paragraph.map(word => {
+        console.log(word);
+        if(top5Keys.includes(word)) {
+            return `<span class = "highlight">${word}</span>`;
+
+        }
+    })
+document.querySelector("#myParagraph").innerHTML=highlightedText.join(" ")
+}
