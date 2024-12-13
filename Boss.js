a02e6310ebf64c71a75c03905ef3b5fe63c44e04
Muhanguzi-boss <bossmuhanguzi@gmail.com>
2024-12-13 15:11:10 +0200

    Todo App beginning

--- Boss.js
+++ Boss.js
@@ -1,3 +1,4 @@
 const arr = ["banana","apple","orange"];
 arr.slice(0,2);
 console.log(arr);
+console.log
---
+++ DOM-Works/GymExercises/DomExercise/app.js
@@ -0,0 +1,5 @@
+
+    const toDoList = document.getElementById("todoList")
+    const newElement = document.createElement("li")
+    newElement.textContent = "Buy Fruits"
+    toDoList.appendChild("newElement")
